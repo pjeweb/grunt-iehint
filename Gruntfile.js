056cc4186d05d4f3bc64e23daa914b9d846d1321
Paul Golmann <p.golmann@neonaut.de>
2014-03-31 14:57:24 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*
+ * grunt-iehint
+ * https://github.com/pjeweb/grunt-iehint
+ *
+ * Copyright (c) 2014 Paul Golmann
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint']);
+
+};
